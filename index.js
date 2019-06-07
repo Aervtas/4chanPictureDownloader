63896b0f53930a01ebad286f1e2ffecd250a016e
archlinux workstation <klin0916@gmail.com>
2019-06-07 17:57:42 -0400

    Create dev branch

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ var io = require('socket.io')(http);
 var express = require('express');
 var app = express();
 
+
+//home
 app.get('/', function(req, res){
   res.send("Hello, world");
 });
 
-
+//localhost:3000, will be changed when hosted on Heroku
 app.listen(3000, function() {
     console.log('Example app listening on port 3000!');
 });
